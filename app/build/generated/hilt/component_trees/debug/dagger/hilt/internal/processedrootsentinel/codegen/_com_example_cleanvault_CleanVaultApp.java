2afe6c7aa018f16e4273fa69c031b6e8f7423f95
Dinesh Sharma <dinesh@splashdata.com>
2026-08-13 09:17:54 +0530

    feat: initial commit for Clean Architecture Android app

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_example_cleanvault_CleanVaultApp.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.example.cleanvault.CleanVaultApp"
-)
-public final class _com_example_cleanvault_CleanVaultApp {
-}
